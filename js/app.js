8ab8cd5f4b8e89e0e982c8886abcdb78c7ae609d
williamd <willy1divx@yahoo.com>
2017-03-02 16:26:46 +0000

    inital file commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+var app = angular.module('myApp',[]);
+
+app.controller('myValues', function($scope){
+	var array = [];
+	var form = {};
+
+	$scope.myVar = false;
+
+	function formatPhoneNumber(s) {
+		  var s2 = (""+s).replace(/\D/g, '');
+		  var m = s2.match(/^(\d{3})(\d{7})$/);
+		  return (!m) ? null : "(" + m[1] + ") " + m[2];
+	}
+	
+	console.log(form);
+	$scope.update = function(form) {
+        $scope.array = array.push(form);
+        console.log(array);
+        $scope.myVar = !$scope.myVar;
+        var result = $scope.form.phone;
+      	results = formatPhoneNumber(result);
+       	form.phone = results;
+        for (var x in form){
+		    if (form[x] === true){
+		    form.nationality = x;
+	    	};
+		};
+        $scope.greeting = form;
+    };    
+});
+
+
+
+	
